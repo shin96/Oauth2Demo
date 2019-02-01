a6891cde9f7f7061fa5787e0fccc2a36e15e2da9
kashis <kashiskthakur@gml.cm>
2019-02-01 21:47:14 +0530

    Feature: fb login

---
+++ src/test/java/com/example/demofbLogin/DemofbLoginApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demofbLogin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemofbLoginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
